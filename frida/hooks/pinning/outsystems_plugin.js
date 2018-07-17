bb44aedc7972b4a961b691573468cc6c8a521880
clviper <claudioandre@gmail.com>
2018-07-17 17:08:53 +0100

    Update outsystems_plugin.js

--- frida/hooks/pinning/outsystems_plugin.js
+++ frida/hooks/pinning/outsystems_plugin.js
@@ -1,7 +1,7 @@
 Java.perform(function x() {
 	try{
-		var OutsystemsPinning = Java.use("com.outsystems.plugins.sslpinning.pinning.X509TrustManagerWrapper");
-		OutsystemsPinning.getPinningHash.overload('android.content.Context', 'java.lang.String').implementation = function(context,folder){
+		var X509TrustManagerWrapper = Java.use("com.outsystems.plugins.sslpinning.pinning.X509TrustManagerWrapper");
+		X509TrustManagerWrapper.getPinningHash.overload('android.content.Context', 'java.lang.String').implementation = function(context,folder){
         		console.log("[-] Outsystems SSLPinning X509TrustManagerWrapper called. Returning null json file");
 			return null;
      		}
